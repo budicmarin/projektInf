9be78e477dacddd8d02a80f212d84642467809ba
budicmarin <budicmarin1@gmail.com>
2024-05-29 11:07:10 +0200

    Sortiranje

--- app.py
+++ app.py
@@ -25,9 +25,33 @@ db.generate_mapping(create_tables=True)
 @app.route('/')
 @db_session
 def index():
-    letovi = Let.select()[:]
-    odredista = Odredište.select()[:]
-    return render_template('index.html', letovi=letovi, odredista=odredista)
+    sort_letovi = request.args.get('sort_letovi', 'id_let')
+    sort_odredista = request.args.get('sort_odredista', 'id_Odredišta')
+
+    letovi_query = Let.select()
+    if sort_letovi == 'datumPolaska':
+        letovi_query = letovi_query.order_by(Let.datumPolaska)
+    elif sort_letovi == 'trajanjeLeta':
+        letovi_query = letovi_query.order_by(Let.trajanjeLeta)
+    elif sort_letovi == 'duljinaLeta':
+        letovi_query = letovi_query.order_by(Let.duljinaLeta)
+    elif sort_letovi == 'odrediste':
+        letovi_query = letovi_query.order_by(Let.id_odredište)
+    else:
+        letovi_query = letovi_query.order_by(Let.id_let)
+
+    odredista_query = Odredište.select()
+    if sort_odredista == 'nazivOdrredišta':
+        odredista_query = odredista_query.order_by(Odredište.nazivOdrredišta)
+    elif sort_odredista == 'državaOdredišta':
+        odredista_query = odredista_query.order_by(Odredište.državaOdredišta)
+    else:
+        odredista_query = odredista_query.order_by(Odredište.id_Odredišta)
+
+    letovi = letovi_query[:]
+    odredista = odredista_query[:]
+
+    return render_template('index.html', letovi=letovi, odredista=odredista, sort_letovi=sort_letovi, sort_odredista=sort_odredista)
 
 @app.route('/letovi', methods=['GET', 'POST'])
 @db_session
